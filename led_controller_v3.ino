cf1d1166978f042d981609e6ac21d6b0ba16f75a
Joseph Commisso <jlcomm07@aol.com>
2020-02-13 12:28:44 -0500

    Updated code for 2020
    Add new serial system and effects

--- led_controller_v3.ino
+++ led_controller_v3.ino
@@ -1,190 +1,172 @@
 #include <FastLED.h>
-#define NUM_LEDS 40
-#define DATA_PIN 3
+#define NUM_LEDS 60
+#define DATA_PIN 7
 
 
 CRGB leds[NUM_LEDS];
 
 
+char newData;
 
-// white is vertical
-// red is horizotal
-char newData = 0;
-char dropDriveState = 0;
+
 void setup() {
-  Serial.begin(115200);   // Start Serial @ 115200 baud
-  Serial.flush();       // Clear  buffer
-
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  Serial.begin(9600);
+  Serial.flush();
+  LEDS.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+  LEDS.setBrightness(150);
+  colorSet(10, 0, 0); 
 }
 
 void loop() {
-  if (Serial.available() > 0) {        // Check buffer
-    newData = Serial.read();            // Save string received
-    Serial.flush();                    // Clear buffer
-
-    switch (newData) {
-
-      case 'a':                        // Idle vertical
-        idleV(20);
-        Serial.println("IDLE-VERT-TESTING");
-        FastLED.show();
-        break;
-
-
-      case 'b':  //Idle horizontal
-        idleH(20);
-        Serial.println("IDLE-HORIZ-TESTING");
-        FastLED.show();
-        break;
-
-      case 'c':                        // Intake
-        Serial.println("INTAKE-TESTING");
-        if (dropDriveState == 1) {
-           RunningLights(0xff,0xff,0xff, 50);  // white
-          FastLED.show();
-        } else {
-           RunningLights(0xff,0,0, 50);        // red
-          FastLED.show();
-        }
-        break;
-
-
-
-      case 'd':                        // Deploy
-        Serial.println("DEPLOY-TESTING");
-        if (dropDriveState == 1) {
-          //deployDD();
-          FastLED.show();
-        } else {
-          deploy();          FastLED.show();
-        }
-        break;
-
-
-
-      case 'e':                        // Drop Drive Down
-        dropDrive();
-        Serial.println("DRIVE-TESTING");
-        dropDriveState = 1; // allows the others to use the horizontal strip for drop drive while drop drive is active
-        FastLED.show();
-
-        break;
-
-
-
-      case 'f':                        // Drop Drive Up
-        Serial.println("DRIVE-UP-TESTING");
-        dropDriveState = 0; // gives full access to the horizontal strip while drop drive is inactive
-        prevoiusState();
-        break;
-
-
-
-      case 'g':                        // Elevator Up
-        elev();
-        Serial.println("ELEV-TESTING");
-        FastLED.show();
-
-        break;
-
-
-      case 'h':                        // Winch
-        winch();
-        Serial.println("WINCH-TESTING");
-        FastLED.show();
-        break;
-
-
-      case 'i':                        // Off
-        Serial.println("OFF-TESTING");
-
-
-        break;
-
-
-      default:
-        Serial.println("Error: Unexpected command");
-    }
+  if (Serial.available() > 0) {
+    newData = Serial.read();
+    Serial.flush();
+    Serial.println(newData);
   }
-
-
+  ledSwitch();
+  delay(1);
 }
 
-void idleH(int SpeedDelay) {
-  byte *c;
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< 10; i++) {
-      c=Wheel(((i * 256 / NUM_LEDS) + j) & 255);
-      setPixel(i, *c, *(c+1), *(c+2));
-    }
-    showStrip();
-    delay(SpeedDelay);
-  }
-}
-
-
-
-void idleH(int SpeedDelay) {
-  byte *c;
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=11; i< 28; i++) {
-      c=Wheel(((i * 256 / NUM_LEDS) + j) & 255);
-      setPixel(i, *c, *(c+1), *(c+2));
-    }
-    showStrip();
-    delay(SpeedDelay);
-  }
-}
-
-
-
-
-
-byte * Wheel(byte WheelPos) {
-  static byte c[3];
-  
-  if(WheelPos < 85) {
-   c[0]=WheelPos * 3;
-   c[1]=255 - WheelPos * 3;
-   c[2]=0;
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   c[0]=255 - WheelPos * 3;
-   c[1]=0;
-   c[2]=WheelPos * 3;
-  } else {
-   WheelPos -= 170;
-   c[0]=0;
-   c[1]=WheelPos * 3;
-   c[2]=255 - WheelPos * 3;
-  }
-
-  return c;
-}
-
-
-void RunningLights(byte red, byte green, byte blue, int WaveDelay) {
-  int Position=0;
-  
-  for(int i=0; i<NUM_LEDS*2; i++)
-  {
-      Position++; // = 0; //Position + Rate;
-      for(int i=0; i<10; i++) {
-        // sine wave, 3 offset waves make a rainbow!
-        //float level = sin(i+Position) * 127 + 128;
-        //setPixel(i,level,0,0);
-        //float level = sin(i+Position) * 127 + 128;
-        setPixel(i,((sin(i+Position) * 127 + 128)/255)*red,
-                   ((sin(i+Position) * 127 + 128)/255)*green,
-                   ((sin(i+Position) * 127 + 128)/255)*blue);
-      }
+void ledSwitch() {
+  switch (newData) {
+    case 'd':
+      Serial.println("disabled");
+      disabled();
+      break;s
       
-      showStrip();
-      delay(WaveDelay);
+    case 'i':
+      Serial.println("idle");
+      idle(25);
+      break;
+    case 's':
+      Serial.println("shoot");
+      shoot();
+      break;
+    case 'c':
+      Serial.println("climb");
+      climb();
+      break;
+    case 'r':
+      Serial.println("red");
+      colorSet(0, 255, 0);
+      break;
+    case 'g':
+      Serial.println("green");
+      colorSet(255, 0, 0);
+      break;
+    case 'b':
+      Serial.println("blue");
+      colorSet(0, 0, 255);
+      break;
+    case 'p':
+      Serial.println("cwred");
+      cwSet(0, 255, 0);
+      break;
+    case 't':
+      Serial.println("cwgreen");
+      cwSet(255, 0, 0);
+      break;
+    case 'u':
+      Serial.println("cwblue");
+      cwSet(0, 0, 255);
+      break;
+    case 'y':
+      Serial.println("cwyellow");
+      cwSet(255, 255, 0);
+      break;
+    case 'o':
+      turnOff();
+      Serial.println("off");
+      fadeall();
+      FastLED.show();
+      break;
+    default:
+      Serial.println("Error: Unexpected command");
   }
+}
+
+void fadeall() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i].nscale8(250);
+  }
+}
+
+void idle(char speed) {
+  static uint8_t hue = 0;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(hue++, 255, 255);
+    FastLED.show();
+    fadeall();
+    delay(speed);
+  }
+  for (int i = (NUM_LEDS) - 1; i >= 0; i--) {
+    leds[i] = CHSV(hue++, 255, 255);
+    FastLED.show();
+    fadeall();
+    delay(15);
+  }
+}
+
+void turnOff() {
+  static uint8_t hue = 0;
+  for (int i = (NUM_LEDS) - 1; i >= 0; i--) {
+    leds[i] = CHSV(0, 0, 0);
+    FastLED.show();
+    fadeall();
+  }
+}
+
+
+void cwSet(char r, char g, char b) {
+  static uint8_t hue = 0;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(r, g, b);
+    FastLED.show();
+    fadeall();
+    delay(5);
+  }
+  for (int i = (NUM_LEDS) - 1; i >= 0; i--) {
+    leds[i] = CRGB(r, g, b);
+    FastLED.show();
+    fadeall();
+    delay(5);
+  }
+}
+
+void shoot() {
+  static uint8_t hue = 0;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(255, 0, 0);
+    FastLED.show();
+    fadeall();
+    delay(4);
+  }
+}
+
+
+void climb() {
+  for (int j = 0; j < 1; j++) {
+    for (int q = 0; q < 3; q++) {
+      for (int i = 0; i < NUM_LEDS; i = i + 3) {
+        int pos = i + q;
+        leds[pos].setRGB(255, 0, 0);
+      }
+      FastLED.show();
+      delay(100);
+      for (int i = 0; i < NUM_LEDS; i = i + 3) {
+        leds[i + q] = CRGB::Black;
+      }
+    }
+  }
+}
+
+void colorSet(char r, char g, char b) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i].setRGB(r, g, b);
+    FastLED.show();
+  }
+}
+
+void disabled() {
+
 }
